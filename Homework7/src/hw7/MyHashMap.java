585f5d0adf3b3880a7eb77b52c8b1795bb1cfbf1
twhorten <troy.whorten@wyo.gov>
2015-05-21 11:27:24 -0600

    Worked on MyHashMap

--- Homework7/src/hw7/MyHashMap.java
+++ Homework7/src/hw7/MyHashMap.java
@@ -1,55 +1,181 @@
 package hw7;
 
+import java.util.ArrayList;
 import java.util.Set;
 
-public class MyHashMap<T1, T2> implements Map61B<Integer, Integer> {
+public class MyHashMap<T1, T2> implements Map61B<T1, T2> {
 
+	private float loadFactor;
+	private int fillCount;
+	private Object[] table;
+	private int initSize = 1;
+	
+	public MyHashMap(){
+		table = new Object[1];
+		this.loadFactor = 1.0f;
+	}
+	
+	public MyHashMap(int initialSize){
+		table = new Object[initialSize];
+		this.loadFactor = 1.0f;
+		this.initSize = initialSize;
+	}
+	
+	public MyHashMap(int initialSize, float loadFactor){
+		if(loadFactor <= 0.1f)
+			throw new IllegalArgumentException("Load Factor must be greater than 0.1");
+		
+		table = new Object[initialSize];
+		this.loadFactor = loadFactor;
+		this.initSize = initialSize;
+	}
+	
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
+		table = new Object[initSize];
 
 	}
 
 	@Override
-	public boolean containsKey(Integer key) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean containsKey(T1 key) {
+		
+		return get(key) != null;
 	}
 
 	@Override
-	public Integer get(Integer key) {
-		// TODO Auto-generated method stub
-		return null;
+	public T2 get(T1 key) {		
+		//choose bin 
+		int index = getBucket(key);
+		if(table[index] == null){	
+			return null;
+		} else {
+			@SuppressWarnings("unchecked")
+			ArrayList<Entry> list = ((ArrayList<Entry>) table[index]);
+			int listIndex = list.indexOf(new Entry(key, null));
+			if(listIndex == -1)
+				return null; 
+			
+			Entry entry = list.get(listIndex);
+			return entry.getValue();
+		}		
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return fillCount;
 	}
 
 	@Override
-	public void put(Integer key, Integer value) {
-		// TODO Auto-generated method stub
-
+	public void put(T1 key, T2 value) {
+		//if load factor will be exceeded, resize table
+		if(tooSmall())
+			resize();
+		
+		//create Entry
+		Entry entry = new Entry(key, value);
+		
+		//choose bin 
+		int index = getBucket(key);
+		
+		addEntryToBucket(table, entry, index);
 	}
 
+	private int getBucket(T1 key) {
+		int index = key.hashCode() % table.length;
+		return index;
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	private void addEntryToBucket(Object[] table, Entry entry, int index) {
+		ArrayList<Entry> list;
+		if(table[index] == null){
+						
+			list = new ArrayList<Entry>();
+			list.add(entry);
+			fillCount += 1;
+			table[index] = list;
+		} else {
+			list = ((ArrayList<Entry>) table[index]);
+			if(!list.contains(entry)){
+							
+				list.add(entry);
+				fillCount += 1;
+			}
+			//this assignment is probably unnecessary... pass by reference
+			//table[index] = list;
+		}
+	}
+
+	private boolean tooSmall() {
+		return ((float)(fillCount + 1)/table.length) > loadFactor;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void resize(){
+		int newLen = (int) (((fillCount + 1) * 2) / loadFactor);
+		fillCount = 0;
+		//System.out.println("Resizing to size " + newLen);
+		//iterate through all the keys and re-map them to the new array
+		Object[] newTable = new Object[newLen];
+		for(Object o : table){
+			if(o != null){
+				ArrayList<Entry> list = (ArrayList<Entry>)o;
+				for(Entry entry : list){
+					int index = getBucket(entry.key);
+					addEntryToBucket(newTable, entry, index);
+				}
+			}	
+		}
+		table = newTable;
+	}
+	
 	@Override
-	public Integer remove(Integer key) {
+	public T2 remove(T1 key) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Integer remove(Integer key, Integer value) {
+	public T2 remove(T1 key, T2 value) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Set<Integer> keySet() {
+	public Set<T1> keySet() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private class Entry {
+		T1 key;
+		T2 value;
+		
+		public Entry(T1 key, T2 value){
+			this.key = key;
+			this.value = value;
+		}
+		
+		public T1 getKey(){
+			return key;
+		}		
+		
+		public T2 getValue(){
+			return value;
+		}	
+		
+	    @Override
+	    public boolean equals(Object o) {
+	        if (o != null && o instanceof MyHashMap.Entry) {
+	        	@SuppressWarnings("unchecked")
+				Entry other = (Entry) o;
+	            return this.getKey() == other.getKey();
+	        }
+	        return false;
+	    }
 	}
 
 }
---
+++ Homework7/src/hw7/MyHashMapTests.java
@@ -0,0 +1,135 @@
+package hw7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyHashMapTests {
+
+	@Test
+	public void Ctor_DefaultCtor_DoesNotCrash() {
+		new MyHashMap<Integer, Integer>();
+	}
+	
+	@Test
+	public void Ctor_SizedCtor_DoesNotCrash() {
+		new MyHashMap<Integer, Integer>(10);
+	}
+	
+	@Test
+	public void Ctor_SizeLoadCtor_DoesNotCrash() {
+		new MyHashMap<Integer, Integer>(10, 2.0f);
+	}
+	
+	@Test
+	public void Put_AddValue_SizeIncreases() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(10, 2.0f);
+		
+		//Act
+		map.put(1, 1);
+		
+		//Assert
+		assertEquals(1, map.size());
+	}
+	
+	@Test
+	public void Put_AddDuplicateValue_SizeStaysSame() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(10, 2.0f);
+		
+		//Act
+		map.put(1, 1);
+		map.put(1, 1);
+		
+		//Assert
+		assertEquals(1, map.size());
+	}
+	
+	@Test
+	public void Put_TwoUniqueKeys_SizeIsTwo() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(10, 2.0f);
+		
+		//Act
+		map.put(1, 1);
+		map.put(2, 1);
+		
+		//Assert
+		assertEquals(2, map.size());
+	}
+	
+	@Test
+	public void Put_ExceedsLoadFactor_ResizeSuccessful() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(1, 0.25f);
+		
+		//Act
+		map.put(1, 1);
+		map.put(2, 1);
+		map.put(3, 1);
+		map.put(4, 1);
+		
+		//Assert
+		assertEquals(4, map.size());
+	}
+	
+	@Test
+	public void Get_NothingInMap_ReturnsNull() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(1, 1.0f);
+		
+		//Act
+		Integer result = map.get(0);
+		
+		//Assert
+		assertNull(result);
+	}
+	
+	@Test
+	public void Get_KeyNotInMap_ReturnsNull() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(1, 1.0f);
+		map.put(1, 1);
+		
+		//Act
+		Integer result = map.get(0);
+		
+		//Assert
+		assertNull(result);
+	}
+
+	
+	@Test
+	public void Get_KeyInMap_ReturnsValue() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(1, 1.0f);
+		map.put(1, 100);
+		
+		//Act
+		int result = map.get(1);
+		
+		//Assert
+		assertEquals(100, result);
+	}
+	
+	@Test
+	public void Get_KeyInMapWithOtherKeys_ReturnsValue() {
+		//Arrange
+		MyHashMap<Integer, Integer> map = new MyHashMap<Integer, Integer>(1, 1.0f);
+		map.put(1, 100);
+		map.put(2, 101);
+		map.put(3, 102);
+		map.put(4, 103);
+		map.put(5, 104);
+		map.put(6, 105);
+		
+		//Act
+		int result1 = map.get(1);
+		int result2 = map.get(6);
+		
+		//Assert
+		assertEquals(100, result1);
+		assertEquals(105, result2);
+	}
+}
