341c4232b1b2841c1a94f6460544a36afb5b6bfa
troy <sabotuer99@gmail.com>
2015-05-21 23:14:44 -0600

    Finished up MapRace

--- Homework7/src/hw7/MapRace.java
+++ Homework7/src/hw7/MapRace.java
@@ -1,4 +1,9 @@
 package hw7;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 // import java.util.HashMap;
 // import java.util.TreeMap;
 // import java.util.Map;
@@ -6,40 +11,80 @@ package hw7;
 
 public class MapRace {
 
+	public static Integer[] keys = new Integer[1];
+	public static Integer[] vals = new Integer[1];
+	
+	public static Integer[] initArray(Integer length){
+		Integer[] arr = new Integer[length];
+		for(int i = 0; i < length; i += 1){
+			//Fisher-Yates "inside-out"
+			int target = (int)(Math.random() * (i + 1));
+			arr[i] = arr[target];
+			arr[target] = i;
+		}
+		//System.out.println("Arrays Initiallized!");
+		return arr;
+	}
+	
     /* Tests the put action the specified number of times. */
-    private static long timePuts61B(Map61B<Integer, Integer> map, 
+    private static long timePuts61B(Map<Integer, Integer> map, 
                 int num_puts, int key_range, int val_range) {
+    	
+    	//System.out.println(num_puts);
+    	
+    	//check to make sure vals is initiallized to the right range
+    	if(vals == null || vals.length != num_puts){
+    		keys = initArray(num_puts);
+    		vals = initArray(num_puts);
+    	}		
+    	
 		Stopwatch sw = new Stopwatch();
 		for(int i = 0; i < num_puts; i += 1){
-			try{
-			map.put((int)(Math.random() * key_range), (int)(Math.random() * val_range));
-			} catch (Exception ex){
-				//gulp
-			}
+			map.put(keys[i], vals[i]);
 		}
 		return (long)(sw.elapsedTime() * 1000);
 
     }
 
     /* Tests the get action the specified number of times. */
-    private static long timeGets61B(Map61B<Integer, Integer> map, 
+    private static long timeGets61B(Map<Integer, Integer> map, 
                 int num_gets, int key_range) {
-        // YOUR CODE HERE
-        return 0;
+    	//check to make sure vals is initiallized to the right range
+    	if(vals == null || vals.length != num_gets){
+    		keys = initArray(num_gets);
+    		vals = initArray(num_gets);
+    	}
+    	
+		Stopwatch sw = new Stopwatch();
+		for(int i = 0; i < num_gets; i += 1){
+			map.get(keys[i]);
+		}
+		return (long)(sw.elapsedTime() * 1000);
     }
 
     /* Tests the get action the specified number of times. */
-    private static long timeRemove61B(Map61B<Integer, Integer> map, 
+    private static long timeRemove61B(Map<Integer, Integer> map, 
                 int num_removes, int key_range) {
-        // YOUR CODE HERE
-        return 0;
+    	//check to make sure vals is initiallized to the right range
+    	if(vals == null || vals.length != num_removes){
+    		keys = initArray(num_removes);
+    		vals = initArray(num_removes);
+    	}
+    	
+		Stopwatch sw = new Stopwatch();
+		for(int i = 0; i < num_removes; i += 1){
+			map.remove(keys[i]);
+		}
+		return (long)(sw.elapsedTime() * 1000);
     }
 
     /* Warms up Java to get the cache hot and ready. If you don't warm up, 
      * you'll see that the first test has an unfair handicap. */
     private static void warmup() {
-        Map61B<Integer, Integer> trashMap1 = new MyHashMap<Integer, Integer>();
-        Map61B<Integer, Integer> trashMap2 = new BSTMap<Integer, Integer>();
+        //Map61B<Integer, Integer> trashMap1 = new MyHashMap<Integer, Integer>();
+        //Map61B<Integer, Integer> trashMap2 = new BSTMap<Integer, Integer>();
+        Map<Integer, Integer> trashMap1 = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> trashMap2 = new TreeMap<Integer, Integer>();
         timePuts61B(trashMap1, MIL, MIL, MIL);
         timePuts61B(trashMap2, MIL, MIL, MIL);
         timeGets61B(trashMap1, MIL, MIL);
@@ -50,8 +95,10 @@ public class MapRace {
 
     private static void run61BTimedTests(int num, int key_range, 
                 int val_range) {
-        Map61B<Integer, Integer> hMap = new MyHashMap<Integer, Integer>();
-        Map61B<Integer, Integer> tMap = new BSTMap<Integer, Integer>();
+        //Map61B<Integer, Integer> hMap = new MyHashMap<Integer, Integer>();
+        //Map61B<Integer, Integer> tMap = new BSTMap<Integer, Integer>();
+        Map<Integer, Integer> hMap = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> tMap = new TreeMap<Integer, Integer>();
 
         // TreeMap puts
         long tPuts = timePuts61B(tMap, num, key_range, val_range);
@@ -73,15 +120,15 @@ public class MapRace {
         String hg = "HashMap " + num + " gets: " + hGets + " ms.";
         System.out.println(hg);
 
-        // HashMap removes
-        long hRemove = timeRemove61B(hMap, num, key_range);
-        String hr = "HashMap " + num + " removes: " + hRemove + " ms.";
-        System.out.println(hr);
-
         // TreeMap removes
         long tRemove = timeRemove61B(tMap, num, key_range);
         String tr = "TreeMap " + num + " removes: " + tRemove + " ms.";
         System.out.println(tr);
+        
+        // HashMap removes
+        long hRemove = timeRemove61B(hMap, num, key_range);
+        String hr = "HashMap " + num + " removes: " + hRemove + " ms.";
+        System.out.println(hr);
     }
 
     public static final String followUp() {
@@ -104,8 +151,9 @@ public class MapRace {
         System.out.println("######## 10 Million ########");
         run61BTimedTests(10 * MIL, 10 * MIL, 10 * MIL);
 
+        //50 million was overkill so I changed to 20 million...
         System.out.println();
-        System.out.println("######## 50 Million ########");
-        run61BTimedTests(50 * MIL, 50 * MIL, 50 * MIL);
+        System.out.println("######## 20 Million ########");
+        run61BTimedTests(20 * MIL, 20 * MIL, 20 * MIL);
     }
 }
